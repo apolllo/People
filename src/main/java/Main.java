dd8146822035effcb6957ecb10331e6264da2d26
Andriy Polodiuk <andriy_polodiuk@epam.com>
2016-07-14 18:40:13 +0300

    Added new fields for Person class, added log4j.properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Person person1 = new Person("Name", "Last Name", "email@gmail.com");
-        Person person2 = new Person("Name1", "Last Name1", "email1@gmail.com");
+        Person person1 = new Person("Name", "Last Name", "email@gmail.com", 0, 0, 0);
+        Person person2 = new Person("Name1", "Last Name1", "email1@gmail.com", 1, 1, 1);
         person1.printDetails();
         person2.printDetails();
     }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -5,17 +5,21 @@ public class Person {
     private String name;
     private String surname;
     private String email;
-    private byte kids;
-    private enum hobby {ACT, COOK, DANCE, SING, READ};
-    private enum sex {MALE, FEMALE};
+    private int kids;
+    private String hobby;
+    private String gender;
+    private enum hobbyList {ACT, COOK, DANCE, SING, READ};
+    private enum genderList {MALE, FEMALE};
 
-    public Person(String name, String surname, String email, byte kids) {
+    public Person(String name, String surname, String email, int kids, int hobbyNumber, int genderNumber) {
         Logger logger = LoggerFactory.getLogger(Person.class);
         setName(name);
         setSurname(surname);
         setEmail(email);
         setKids(kids);
-        logger.info("Hello World");
+        setHobby(hobbyNumber);
+        setGender(genderNumber);
+        logger.debug("Hello World");
     }
 
     public String getName() {
@@ -42,13 +46,33 @@ public class Person {
         this.email = email;
     }
 
-    public byte getKids() {
+    public int getKids() {
         return kids;
     }
 
-    public void setKids(byte kids) {
+    public void setKids(int kids) {
         if (kids >= 0 && kids < 10) { this.kids = kids; }
         else { System.out.println("log");}
+    }
+    
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(int hobbyNumber) {
+        if (hobbyNumber >= 0 && hobbyNumber < hobbyList.values().length) { 
+        	hobby = hobbyList.values()[hobbyNumber].toString(); 
+        } else { System.out.println("log");}
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(int genderNumber) {
+        if (genderNumber >= 0 && genderNumber < genderList.values().length) { 
+        	gender = genderList.values()[genderNumber].toString(); 
+        } else { System.out.println("log");}
     }
 
     public void printDetails() {
@@ -56,5 +80,7 @@ public class Person {
         System.out.println(getSurname());
         System.out.println(getEmail());
         System.out.println(getKids());
+        System.out.println(getHobby());
+        System.out.println(getGender());
     }
 }
